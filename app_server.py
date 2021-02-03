343914353d9f3ca40c830e14688f4f539a4622a6
charantej224 <charantej.career@gmail.com>
2021-02-02 23:41:39 -0500

    policy and council boundary mapping additions

--- app_server.py
+++ app_server.py
@@ -5,33 +5,46 @@ from models.geo_boundary_model import BoundaryMapper
 from models.bert_predictive_models import PredictiveModel
 from utilities.app_logger import AppLogger
 import os
+from flask_ngrok import run_with_ngrok
 
 logger = AppLogger.getInstance()
 app = Flask(__name__)
+run_with_ngrok(app)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 root = "/home/charan/DATA/311_Data/311_Server_Files"
 neigh = os.path.join(root, "Kansas_City_Neighborhood_Boundaries.geojson")
 block = os.path.join(root, "1672_Block_Group_Shape.json")
-boundary_mapper = BoundaryMapper(block, neigh)
+police = os.path.join(root, "Police Divisions.geojson")
+council = os.path.join(root, "Council Districts.geojson")
+
+boundary_mapper = BoundaryMapper(block, neigh, police, council)
 pred_model = PredictiveModel(root)
 
 
-@app.route('/getPrediction', methods=['POST'])
+@app.route('/getPrediction', methods=['GET', 'POST'])
 @cross_origin()
 def get_predictions():
-    logger.debug("Making search_query call.")
-    description = request.json['description']
+    if request.method == 'POST':
+        logger.debug("Making search_query call.")
+        description = request.json['description']
+    elif request.method == 'GET':
+        logger.debug("Making GET call.")
+        description = request.args.get('description')
     result = pred_model.inference_predictive_models(description)
     return jsonify(result), 200
 
 
-@app.route('/getGeoLocations', methods=['POST'])
+@app.route('/getGeoLocations', methods=['GET', 'POST'])
 @cross_origin()
 def get_geo_locations():
-    logger.debug("Making search_query call.")
-    latitude, longitude = request.json['latitude'], request.json['longitude']
+    if request.method == 'POST':
+        logger.debug("Making search_query POST call.")
+        latitude, longitude = request.json['latitude'], request.json['longitude']
+    elif request.method == 'GET':
+        logger.debug("Making search_query GET call.")
+        latitude, longitude = float(request.args.get('latitude')), float(request.args.get('longitude'))
     boundaries = boundary_mapper.get_boundaries(latitude, longitude)
     return jsonify(boundaries), 200
 
--- models/geo_boundary_model.py
+++ models/geo_boundary_model.py
@@ -2,11 +2,13 @@ from utilities.file_utils import read_json
 
 
 class BoundaryMapper:
-    def __init__(self, input_block_group, input_neighborhood):
+    def __init__(self, input_block_group, input_neighborhood, police_boundary, council_boundary):
         self.input_block_group = read_json(input_block_group)
         self.input_neighborhood = read_json(input_neighborhood)
+        self.police_boundary = read_json(police_boundary)
+        self.council_boundary = read_json(council_boundary)
 
-    def map_block_group(self, latitude, longitude):
+    def map_block_group(self, latitude, longitude, response):
         for each in self.input_block_group.keys():
             right_top, right_bottom = False, False
             left_top, left_bottom = False, False
@@ -22,11 +24,12 @@ class BoundaryMapper:
                     left_top = True
                 if right_top and right_bottom and left_top and left_bottom:
                     print(f'block id : {each}')
-                    return each
-        return "N/A"
+                    response["block_id"] = each
+                    return
+        return
 
-    def map_neighborhood(self, latitude, longitude):
-        for each in self.input_neighborhood['features']:
+    def map_neighborhood(self, input_boundary, latitude, longitude, key_list, response):
+        for each in input_boundary['features']:
             right_top, right_bottom = False, False
             left_top, left_bottom = False, False
             list_geo_points = each['geometry']['coordinates'][0][0]
@@ -40,10 +43,19 @@ class BoundaryMapper:
                 if latitude > each_point[1] and longitude < each_point[0]:
                     left_top = True
                 if right_top and right_bottom and left_top and left_bottom:
-                    return each["properties"]["nbhname"], each["properties"]['nbhid']
-        return "N/A", "N/A"
+                    for key in key_list:
+                        if key in each["properties"]:
+                            response[key] = each["properties"][key]
+                    return
+        return
 
     def get_boundaries(self, in_latitude, in_longitude):
-        block_id = self.map_block_group(in_latitude, in_longitude)
-        nbh_name, nbh_id = self.map_neighborhood(in_latitude, in_longitude)
-        return {"block_id": block_id, "nbh_name": nbh_name, "nbh_id": nbh_id}
+        key_list = ["nbhname", "block_id", "nbhid", "division", "divisionname", "indistrict", "atlarge", "district"]
+        response = {}
+        for key in key_list:
+            response[key] = "N/A"
+        self.map_block_group(in_latitude, in_longitude, response)
+        self.map_neighborhood(self.input_neighborhood, in_latitude, in_longitude, key_list, response)
+        self.map_neighborhood(self.police_boundary, in_latitude, in_longitude, key_list, response)
+        self.map_neighborhood(self.council_boundary, in_latitude, in_longitude, key_list, response)
+        return response
